b25414177b68b8b441904dbf2606199cab135061
Bruno Brito <batioe.std@gmail.com>
2021-10-12 21:30:41 -0300

    AsyncTask

--- app/src/main/java/com/deny/tarefasassincronas/MainActivity.java
+++ app/src/main/java/com/deny/tarefasassincronas/MainActivity.java
@@ -2,13 +2,53 @@ package com.deny.tarefasassincronas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+
+    }
+
+    public void iniciarAsyncTask(View view){
+
+    }
+
+    /*
+    1 -> Parametro a ser passado para a classe
+    2 -> Tipo de valor que será utilizado para o progresso da tarefa
+    3 -> Retorno apos a tarefa ser finalizada
+     */
+    class MyAsyncTask extends AsyncTask<Integer, Integer, String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Integer... integers) {
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
 }
